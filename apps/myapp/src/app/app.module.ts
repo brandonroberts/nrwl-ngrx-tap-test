bb0aa7906e8d28e7e79b15eaf18e5b8c0455c51c
Brandon Roberts <brandon@nrwl.io>
2019-11-04 12:47:53 -0600

    add ngrx and app effects

--- apps/myapp/src/app/app.module.ts
+++ apps/myapp/src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot({}, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }), EffectsModule.forRoot([])],
   providers: [],
   bootstrap: [AppComponent]
 })
